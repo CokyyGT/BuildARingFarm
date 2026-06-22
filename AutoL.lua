c314675a4929d36179809877bc0a17eee35ead98
CokyyGT <gtpsmarket0@gmail.com>
2026-06-22 19:05:15 +0700

    Update AutoL.lua

--- AutoL.lua
+++ AutoL.lua
@@ -1,3 +1,8 @@
+-- Cleanup
+if game.CoreGui:FindFirstChild("AutoRollUI") then
+    game.CoreGui.AutoRollUI:Destroy()
+end
+
 local Players    = game:GetService("Players")
 local UIS        = game:GetService("UserInputService")
 local HttpService = game:GetService("HttpService")
@@ -33,21 +38,22 @@ local TARGET_RARITIES  = {}
 -- Target seed by name (OR dengan rarity)
 local SEED_NAMES = {
     { name = "Godspore",            enabled = false },
-        { name = "Seraphim Spire",      enabled = false },
+    { name = "Sundrop Seraphina",   enabled = false },
+    { name = "Seraphim Spire",      enabled = false },
     { name = "Aethercoil",          enabled = false },
     { name = "Obsidian Figwort",    enabled = false },
     { name = "Voidglass Heliconia", enabled = false },
-        { name = "Titan Arum",          enabled = false },
+    { name = "Solstice Snapdragon", enabled = false },
+    { name = "Titan Arum",          enabled = false },
     { name = "Aurora Lotus",        enabled = false },
     { name = "Ember Fruit",         enabled = false },
-        { name = "Ghost Pepper",        enabled = false },
+    { name = "Tideglass Orchid",    enabled = false },
+    { name = "Ghost Pepper",        enabled = false },
     { name = "Papaya",              enabled = false },
     { name = "Durian",              enabled = false },
     { name = "Silver Artichoke",    enabled = false },
     { name = "Dragon Scale Aloe",   enabled = false },
-    { name = "Elder Dragonroot",    enabled = false },,
-    { name = "Pepper",     enabled = false },
-    { name = "Void Fruit",  enabled = false }
+    { name = "Elder Dragonroot",    enabled = false },
 }
 local TARGET_SEED_NAMES = {}
 for _, s in ipairs(SEED_NAMES) do
